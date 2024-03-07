74d885c6bb34e32f8b3bd4d4dab71da37436cb17
Your Name <mrtennizman@gmail.com>
2024-03-07 03:31:51 +0000

    env setup

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const MEXC = require("./MEXC JS SDK API/package/")
+require('dotenv').config()
+
 
 const client = new MEXC.Spot()
+client.config.apiKey = process.env.apiKey
+client.config.apiSecret = process.env.apiSecret
 
 const trades = client.trades("BTCUSDT", { limit: 5 })
 console.log(trades)
