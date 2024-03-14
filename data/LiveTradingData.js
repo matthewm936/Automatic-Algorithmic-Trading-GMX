fcc9d1d599fb2c83568e6e98e3e5ed724da5f752
Your Name <mrtennizman@gmail.com>
2024-03-14 18:03:38 +0000

    adding list of mexc top 600 pairs

--- data/LiveTradingData.js
+++ data/LiveTradingData.js
@@ -9,23 +9,24 @@ client.config.apiSecret = process.env.apiSecret;
 logFile = '/home/johnsmith/Trading/Algorithmic-Trading/data/data log.txt';
 
 Promise.all([
-    client.ticker24hr("BTCUSDT"),
-    client.ticker24hr("ETHUSDT"),
-    client.ticker24hr("BNBUSDT"),
-    client.ticker24hr("ADAUSDT"),
-    client.ticker24hr("XRPUSDT"),
-    client.ticker24hr("SOLUSDT"),
-    client.ticker24hr("DOTUSDT"),
+	client.ticker24hr("BTCUSDT"),
+	client.ticker24hr("ETHUSDT"),
+	client.ticker24hr("BNBUSDT"),
+	client.ticker24hr("ADAUSDT"),
+	client.ticker24hr("XRPUSDT"),
+	client.ticker24hr("SOLUSDT"),
+	client.ticker24hr("DOTUSDT"),
 ]).then(([btcData, ethData, bnbData, adaData, xrpData, solData, dotData]) => {
-    const data = {
-        BTCUSDT: btcData,
-        ETHUSDT: ethData,
-        BNBUSDT: bnbData,
-        ADAUSDT: adaData,
-        XRPUSDT: xrpData,
-        SOLUSDT: solData,
-        DOTUSDT: dotData,
-    };
+	const data = {
+		BTCUSDT: btcData,
+		ETHUSDT: ethData,
+		BNBUSDT: bnbData,
+		ADAUSDT: adaData,
+		XRPUSDT: xrpData,
+		SOLUSDT: solData,
+		DOTUSDT: dotData,
+	};
+	
 
 	const writeFilePromise = new Promise((resolve, reject) => {
 		fs.writeFile('/home/johnsmith/Trading/Algorithmic-Trading/data/prices.json', JSON.stringify(data, null, 4), (err) => {
@@ -36,9 +37,9 @@ Promise.all([
 
 	writeFilePromise.then(() => {
 		const logMessage = [
-			'Successfully Ran LiveTradingData.js',
-			`\tISO Time: ${new Date().toISOString()}`,
-			`\tUnix Timestamp: ${Date.now()}`
+			'Ran LiveTradingData.js',
+			`\tISO: ${new Date().toISOString()}`,
+			`\tUnix: ${Date.now()}`
 		].join('\n');
 
 		fs.appendFile(logFile, `${logMessage}\n\n`, (err) => {
